3088b216639a3bf6cad1a8d1397e45f7087e24bf
Ezequiel <ezequiel.maranda@gmail.com>
2025-03-08 02:33:01 -0300

    Remove WinAQMS driver and update module exports

--- drivers/__init__.py
+++ drivers/__init__.py
@@ -1,5 +1,4 @@
 from .bam1020 import last_hour_bam1020
-from .winaqms import WinAQMS
 from .davis_vantage_pro2 import DavisVantagePro2
 
-__all__ = ["last_hour_bam1020", "WinAQMS", "DavisVantagePro2"]
+__all__ = ["last_hour_bam1020", "DavisVantagePro2"]
