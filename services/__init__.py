0be69d180a449b538417a5a3890079261d26a43d
Ezequiel <ezequiel.maranda@gmail.com>
2025-03-07 23:21:56 -0300

    Add CSVPublisher to the module exports in services

--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,4 @@
 from .data_collector import DataCollector, SensorConfig, CollectorState, Sensor
+from .publisher import CSVPublisher
 
-__all__ = ["DataCollector", "CollectorState", "SensorConfig", "Sensor"]
+__all__ = ["DataCollector", "CollectorState", "SensorConfig", "Sensor", "CSVPublisher"]
